1435d6acadc92ee7a3a90539fd73724c98b512a3
jonathan-gartland <jonathan-gartland@gmail.com>
2024-11-11 16:08:21 -0500

    feat(setup): start adding tests

---
+++ src/test/mpn.test.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { describe } from "@jest/globals";
+
+describe("MPN Lookup", () => {
+  test("MPN Lookup", () => {
+    expect(1).toEqual(1);
+    // const { getByText } = render(<MyComponent testtype={'quanti2k'}/>);
+    // expect(getByText("QuantiTray2000® MPN")).toBeInTheDocument();
+  });
+});
