88b042cc28275a7656631fd6d13087d2012efcb6
jonathan-gartland <jonathan-gartland@gmail.com>
2024-11-11 16:08:45 -0500

    feat(setup): start adding tests

--- src/test/mpn.test.ts
+++ src/test/mpn.test.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import { describe } from "@jest/globals";
+// import { describe } from "@jest/globals";
 
 describe("MPN Lookup", () => {
   test("MPN Lookup", () => {
