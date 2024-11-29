ec03f6dccaff3d95853308c4cd097dc6db6f16e8
jonathan-gartland <jonathan-gartland@gmail.com>
2024-11-28 19:01:15 -0500

    chore(test): add more stubs, get ready for real tests

--- cypress/e2e/mpn-nextjs/qt.cy.ts
+++ cypress/e2e/mpn-nextjs/qt.cy.ts
@@ -1,23 +1,41 @@
 /// <reference types="cypress" />
 
 describe("QT test stub", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-    cy.get("button[id='quantitray']").click();
+  describe("Validate UI components", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000");
+      cy.get("button[id='quantitray']").click();
+    });
+
+    it("should display the title", () => {
+      cy.contains("QuantiTray® MPN");
+    });
+
+    it("should display text: Positive Wells", () => {
+      cy.get('[data-test-id="pos-well-title"]').should("be.visible");
+    });
+
+    it("should display zero in the input field", () => {
+      cy.get('input[type="numeric"]').should("have.value", 0);
+    });
   });
 
-  it("should display the title", () => {
-    cy.contains("QuantiTray® MPN");
-  });
+  describe("Functional Validation", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000");
+      cy.get("button[id='quantitray']").click();
+    });
 
-  it("should display text: Positive Wells", () => {
-    cy.get('[data-test-id="pos-well-title"]').should("be.visible");
-  });
+    it("should display the title", () => {
+      cy.contains("QuantiTray® MPN");
+    });
 
-  it("should display zero in the input field", () => {
-    cy.get('input[type="numeric"]').should("have.value", 0);
+    it("should display text: Positive Wells", () => {
+      cy.get('[data-test-id="pos-well-title"]').should("be.visible");
+    });
+
+    it("should display zero in the input field", () => {
+      cy.get('input[type="numeric"]').should("have.value", 0);
+    });
   });
-  //
-  // it('should display the search button', () => {
-  //     cy.get('button[type="submit"]
 });
