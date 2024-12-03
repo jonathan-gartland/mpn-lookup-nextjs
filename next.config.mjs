0f4735380139cc616ceedcab071d8e9a1b020fab
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-02 19:32:10 -0500

    chore(config): fix project name in config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/mpn-lookup-nextjs",
 
   /**
    * Disable server-based image optimization. Next.js does not support
