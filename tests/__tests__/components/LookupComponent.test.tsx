5567693863e974ab5b81b1537b0f963aa6e55a8a
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-10 20:43:48 -0500

    chore: cleanup dummy test

--- tests/__tests__/components/LookupComponent.test.tsx
+++ tests/__tests__/components/LookupComponent.test.tsx
@@ -27,6 +27,5 @@ describe("LookupComponent", () => {
     expect(screen.getByText(/QuantiTray2000/i)).toBeInTheDocument();
     expect(largeInput).toHaveValue("10");
     expect(smallInput).toHaveValue("5");
-    expect(1).toBe(2-1);
   });
 });
