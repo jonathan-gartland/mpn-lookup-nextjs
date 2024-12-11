a32a7d9e8e4bb0bbb97021647cfb2838d02eb30c
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-11 11:44:07 -0500

    chore: add tests

--- tests/__tests__/components/Home.test.tsx
+++ tests/__tests__/components/Home.test.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
-import LookupComponent from "@/components/LookupComponent";
+import Home from "@/pages/index";
 
-describe("LookupComponent", () => {
+describe("Home", () => {
   it("renders MPN Lookup Home view", () => {
-    render(<LookupComponent testtype="quanti" />);
-    const header = screen.getByRole("heading", { name: /MPN : < 1.0/i });
+    render(<Home />);
+    const header = screen.getByRole("heading", { name: /MPN Lookup/i });
     expect(header).toBeInTheDocument();
-    expect(header).toHaveTextContent("MPN : < 1");
   });
 });
